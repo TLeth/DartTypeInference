c42a72a8054c119cfa5bddbc26c0af022e6bda2d
Troels Leth Jensen <tleth@Troelss-MacBook-Pro.local>
2014-09-11 11:14:38 +0200

    local variables

--- bin/LocalVariableVisitor.dart
+++ bin/LocalVariableVisitor.dart
@@ -1,42 +1,122 @@
+library typeanalysis.Local;
+
+import 'package:analyzer/src/generated/source.dart';
 import 'package:analyzer/src/generated/ast.dart';
-import 'element.dart';
 
+import 'element.dart' as Our;
+import 'engine.dart';
 
-class VariableVisitor extends GeneralizingAstVisitor {
+import 'dart:collection';
 
-  Map<String, VariableElement> scope = {};
+_openNewScope(scope, k) {
+  Map curr = {};
+  curr.addAll(scope);
+  var ret = k(scope);
+  scope.clear();
+  scope.addAll(curr);
+  return ret;
+}
 
-  resolveVariables(ElementAnalysis analysis) {
-    var result = {};
-    analysis.forEach((k, v) => result.add(k, v));
+class SomethingSomethingVisitor extends Our.RecursiveElementVisitor {
+
+  Map<String, Our.Element> scope = <String, Our.Element>{};
+  Map<SimpleIdentifier, Our.VariableElement> declaredVariables = {};
+
+  var engine;
+  SomethingSomethingVisitor(this.engine);
+
+  void visitElementAnalysis(Our.ElementAnalysis element) {
+    var res = <Source, Our.SourceElement>{};
+    element.sources.forEach((source, sourceElement) =>
+        res.add(source, sourceElement.accept(this)));
+  }
+  
+  void visitSourceElement(Our.SourceElement element) {
+    //Add to scope:
+    //element.library;
+    //element.variables;
+    //element.classes;
+    //element.functions;
+    declaredVariables.clear();
+    declaredVariables.addAll(element.declaredVariables);
+    element.functions.values.forEach((f) { f.accept(this); });
+    element.classes.forEach((c) { c.accept(this); });
+        
+    Map<Identifier, Our.Element> references = {};
     
+    _openNewScope(<String, Our.Element>{}, (scope) => 
+        element.ast.accept(
+            new LocalVariableVisitor(this.engine, 
+                                     scope, 
+                                     this.declaredVariables,
+                                     references)));
+  }
+  
+  void visitFunctionElement(Our.FunctionElement element) {
+    declaredVariables.addAll(element.declaredVariables);
+  }
+  
+  void visitClassElement(Our.ClassElement element) {
+    declaredVariables.addAll(element.declaredVariables);
+    element.methods.forEach((m) => m.accept(this));
+  }
+      
+  void visitMethodElement(Our.MethodElement element) {
+    declaredVariables.addAll(element.declaredVariables);
+  }
+}
+
+
+class LocalVariableVisitor extends GeneralizingAstVisitor {
+
+  Map<Identifier, Our.Element> references;
+  Map<Identifier, Our.VariableElement> declaredVariables;
+  Map<Identifier, Our.VariableElement> declaredFunctions;
+  Map<String, Our.Element> scope;
+  
+  var engine;
+
+  
+  LocalVariableVisitor(this.engine, this.scope, this.declaredVariables, this.references);
+
+
+  visitBlock(Block node) {
+    _openNewScope(scope, (_) => super.visitBlock(node));
   }
 
-  @override
-    visitSimpleIdentifier(SimpleIdentifier node) {
-    print('${node.toString()} -> ${scope[node.toString()]}');
-    visitIdentifier(node);
+  visitVariableDeclaration(VariableDeclaration node) {
+    scope[node.name.toString()] = declaredVariables[node.name];
+    super.visitVariableDeclaration(node);
   }
 
-  @override
-    visitPrefixedIdentifier(PrefixedIdentifier node) {
-    visitIdentifier(node);
+  visitFunctionDeclaration(FunctionDeclaration node) {
+    if (node.name != null) {
+      scope[node.name.toString()] = declaredFunctions[node];
+    }
+    super.visitFunctionDeclaration(node);
   }
+  
+  
+  visitSimpleIdentifier(SimpleIdentifier node) {
     
-  @override
-    visitVariableDeclaration(VariableDeclaration node) {
-    scope[node.name.toString()] = 123;
-    visitDeclaration(node);
+    var element = scope[node.name.toString()];
+    if (element != null) {
+      references[node] = element;
+    } else {
+      this.engine.errors.addError(new EngineError('Couldnt resolve ${node.name}!'));
+    }
   }
 
-  @override
-    visitVariableDeclarationList(VariableDeclarationList node) {
-    visitNode(node);
+
+  //Only resolve the prefix.
+  //TODO handle library 'as ...' imports.
+  visitPrefixedIdentifier(PrefixedIdentifier node) {
+    node.safelyVisitChild(node.prefix, this);
   }
 
-  @override
-    visitVariableDeclarationStatement(VariableDeclarationStatement node) {
-    visitStatement(node);
+  //Only try to resolve the target.
+  visitPropertyAcces(PropertyAccess node) {
+    node.safelyVisitChild(node.target, this);
   }
 
 }
