4d3af3def87a2aa24c29d3cbdc88fdb4cbb27e51
Troels Leth Jensen <tleth@Troelss-MacBook-Pro.local>
2014-09-15 15:00:17 +0200

    commit

--- examples/ImportExample/bin/Runit.dart
+++ examples/ImportExample/bin/Runit.dart
@@ -1,41 +1,29 @@
-
+/*
 import 'dart:io';
 import 'package:analyzer/src/generated/ast.dart';
 import 'package:analyzer/src/generated/constant.dart';
+*/
 
-class Test{
-  Test(){
-    var n;
+class Foo {
+  foo_b(var foo_c){
+    
+    return foo_a + foo_c;
   }
+  var foo_a;
   
 }
 
-void main() {
-  int a = 3;
-  int b = 4;
-  var c = 4.0;
-  var d = 3.5;
-  
-  var e = a + b;
-  
-  var f = c + d;
-  var g = c + a;
-  
+foo(var a){
+  return a;
+}
 
- 
+void main() {
+  var c = 2;
+  var d = foo(c);
   
-  /*
-  print(g is num);  //true
-  print(g is double); //true
-  print(g is int);  //false
-  print(e is double); //false
-  print(e is num);  //true
+  var e = (var f){
+    return f+c;
+  };
   
-  */
-  
-  /*var corp = new Corporation();
-  var theBoss = new Manager("Anders", corp);
-  
-  ['Jytte Hansen','Frede Abe','Chris Abekat'].forEach((name) => corp.hire(new Person(name, 10000))); 
-  print(corp.employees);*/
+  var g = new Foo();
 }
