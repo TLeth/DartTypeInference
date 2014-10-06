67b3ddedc6fa661a2a77fc8be1b4d9e071c531ca
Troels Leth Jensen <troelslethjensen@gmail.com>
2014-10-06 10:47:24 +0200

    solves variables correctly - bug with functions

--- bin/name_resolver.dart
+++ bin/name_resolver.dart
@@ -7,26 +7,23 @@ import 'engine.dart';
 
 
 _openNewScope(scope, k) {
+
+
   Map curr = {};
   curr.addAll(scope);
+
   var ret = k(scope);
+
   scope.clear();
   scope.addAll(curr);
   return ret;
-}
-
-_addToScope(scope, bindings, k) {
-  return _openNewScope(scope, (scope) {
-    scope.addAll(bindings);
-    k(scope);
-  });
 }
 
 //TODO (jln): factories should be handled here. in ConstructorName there should be resolved what is a prefix of another library, and what is a factory call.
 
 class IdentifierResolver extends Our.RecursiveElementVisitor {
 
-  Map<Our.Name, Our.NamedElement> declaredElements = {};
+  Map<AstNode, Our.NamedElement> declaredElements = {};
   Our.LibraryElement currentLibrary;
   var engine;
 
@@ -41,6 +38,7 @@ class IdentifierResolver extends Our.RecursiveElementVisitor {
   }
   
   void visitSourceElement(Our.SourceElement element) {
+
     if (element.source != this.engine.entrySource) return;
     
     declaredElements.clear();
@@ -48,13 +46,11 @@ class IdentifierResolver extends Our.RecursiveElementVisitor {
     
     visitBlock(element);
     
-    Map<String, Our.NamedElement> scope = {};
     ScopeVisitor visitor = new ScopeVisitor(this.engine, 
-                                            scope, 
                                             this.declaredElements,
                                             element.resolvedIdentifiers);
 
-    _openNewScope(scope, (scope) => element.ast.accept(visitor));
+    element.ast.accept(visitor);
   }
   
   void visitBlockList(List<Our.Block> blocks) {
@@ -64,7 +60,10 @@ class IdentifierResolver extends Our.RecursiveElementVisitor {
   }
   
   void visitBlock(Our.Block block) {
-    this.declaredElements.addAll(block.declaredElements);
+    
+    block.declaredElements.values.forEach((Our.Element elem) {
+      this.declaredElements[elem.ast] = elem;
+    });
     
     visitBlockList(block.nestedBlocks);      
   }
@@ -73,59 +72,66 @@ class IdentifierResolver extends Our.RecursiveElementVisitor {
 
 class ScopeVisitor extends GeneralizingAstVisitor {
 
+  Map<AstNode, Our.NamedElement> declaredElements;
   Map<Expression, Our.NamedElement> references = {};
-  Map<Our.Name, Our.NamedElement> declaredElements;
   Map<String, Our.NamedElement> scope;
+
   
   var engine;
   Our.ClassElement _currentClass = null;
-  
-  List<Map<String,Our.NamedElement>> scopes = <Map<String,Our.NamedElement>>[];
     
-  _enterScope(){
-    scopes.add(scope);
-    scope = new Map<String, Our.NamedElement>.from(scope);
+  ScopeVisitor(this.engine, this.declaredElements, this.references) {
+    this.scope = {};
   }
-  
-  _leaveScope(){
-    scope = scopes.removeLast();
-  }
-  
-  ScopeVisitor(this.engine, 
-               this.scope, 
-               this.declaredElements, 
-               this.references); 
 
   visitBlock(Block node) {
-    _enterScope();
-    super.visitBlock(node);
-    _leaveScope();
+    _openNewScope(scope, (_) =>
+        super.visitBlock(node));
+
+    //  _enterScope();
+    //super.visitBlock(node);
+    // _leaveScope();
   }
 
   visitFormalParameter(FormalParameter node) {
-    this.scope[node.identifier.toString()] = this.declaredElements[new Our.Name.FromIdentifier(node.identifier)];
+    this.scope[node.identifier.toString()] = this.declaredElements[node];
     super.visitFormalParameter(node);
   }
   
   visitClassDeclaration(ClassDeclaration node){
-    this.scope["this"] = this.declaredElements[new Our.Name.FromIdentifier(node.name)];
+    this.scope["this"] = this.declaredElements[node];
     super.visitClassDeclaration(node);
     this.scope.remove("this");
   }
 
   visitVariableDeclaration(VariableDeclaration node) {
-    this.scope[node.name.toString()] = this.declaredElements[new Our.Name.FromIdentifier(node.name)];
-    super.visitVariableDeclaration(node);
+    if (node.initializer != null) node.initializer.accept(this);
+    this.scope[node.name.toString()] = this.declaredElements[node];
+    if (node.name != null) node.name.accept(this);
   }
   
   visitFunctionDeclaration(FunctionDeclaration node) {
     if (node.name != null) {
-      this.scope[node.name.toString()] = this.declaredElements[new Our.Name.FromIdentifier(node.name)];
+      this.scope[node.name.toString()] = this.declaredElements[node];
     }
-    super.visitFunctionDeclaration(node);
+
+    if (this.declaredElements[node] == null) {
+      print('failed for ${node} (${node.hashCode})');
+      this.declaredElements.keys.forEach((key){
+        print('${key} (${key.hashCode})');
+        
+      });
+
+    }
+    
+    _openNewScope(scope, (_) {
+      super.visitFunctionDeclaration(node);      
+    });
+
   }
   
   visitSimpleIdentifier(SimpleIdentifier node) {
+
     var element = scope[node.name.toString()];
     if (element != null) {
       references[node] = element;
