0b3c1ad61a8c0408c6bf41fbbd869355fe57d679
Troels Leth Jensen <troelslethjensen@gmail.com>
2014-10-16 15:40:37 +0200

    tests for conditionals

---
+++ tests/dowhile_cond.dart
@@ -0,0 +1,7 @@
+library test.dowhile_cond;
+
+void t(bool arg) {
+  do {} while (arg);
+}
+
+
--- tests/tests.dart
+++ tests/tests.dart
@@ -67,6 +67,11 @@ import 'classes.dart';
 import 'getter.dart';
 import 'setter.dart';
 import 'superexp.dart';
+import 'for_cond.dart';
+import 'while_cond.dart';
+import 'dowhile_cond.dart';
+import 'if_cond.dart';
+import 'cond_exp.dart';
 
 
 void main() {}
