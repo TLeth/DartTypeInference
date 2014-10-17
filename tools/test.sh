69cbe49a673df0202acc1666868709e28346db29
Troels Leth Jensen <troelslethjensen@gmail.com>
2014-10-17 13:39:02 +0200

    test

--- tools/test.sh
+++ tools/test.sh
@@ -22,28 +22,61 @@ done
 rm -f tests/output/*.dart
 cp tests/.stripped/*.dart tests/output/
 
-dart ./bin/analyze.dart --dart-sdk ${dartDir%bin/dart} tests/output/tests.dart
+if [ $# -eq 0 ]; then
 
-(for f in $(ls tests/cases/)
-do
-  dartfmt -w tests/output/$f
+    dart ./bin/analyze.dart --dart-sdk ${dartDir%bin/dart} tests/output/tests.dart > /dev/null
+    
+    (for f in $(ls tests/cases/); do
+        
+        dartfmt -w tests/output/$f
+        
+        if [ $(diff <(cat tests/cases/$f) <(cat tests/output/$f) | wc -l) -eq 0 ]; then
+            echo -e $f '\033[0;32mPass\033[0m'
+            
+        else 
+            echo -e $f '\033[0;31mFail\033[0m'
+            echo $f >> log
+            echo "------------------------------" >> log
+            echo "got" >> log
+            cat tests/output/$f >> log
+            printf "\n\n" >> log
+            echo "expected" >> log
+            cat tests/cases/$f >> log
+            printf "\n\n" >> log
+            echo "diff" >> log
+            diff <(cat tests/cases/$f) <(cat tests/output/$f) >> log
+            printf "\n\n" >> log
+        fi
+        
+        done)|column -t
+else
+    (for arg in $*; do
+        
+        if [ -f tests/cases/$arg.dart ]; then
+        
+            dart ./bin/analyze.dart --dart-sdk ${dartDir%bin/dart} tests/output/$arg.dart > /dev/null
+            dartfmt -w tests/output/$arg.dart
+            
+            if [ $(diff <(cat tests/cases/$arg.dart) <(cat tests/output/$arg.dart) | wc -l) -eq 0 ]; then
+                echo -e $arg.dart '\033[0;32mPass\033[0m'
+                
+            else 
+                echo -e $arg.dart '\033[0;31mFail\033[0m'
+                echo $arg.dart >> log
+                echo "------------------------------" >> log
+                echo "got" >> log
+                cat tests/output/$arg.dart >> log
+                printf "\n\n" >> log
+                echo "expected" >> log
+                cat tests/cases/$arg.dart >> log
+                printf "\n\n" >> log
+                echo "diff" >> log
+                diff <(cat tests/cases/$arg.dart) <(cat tests/output/$arg.dart) >> log
+                printf "\n\n" >> log
+            fi
+        fi
 
-  if [ $(diff <(cat tests/cases/$f) <(cat tests/output/$f) | wc -l) -eq 0 ]
-  then echo -e $f '\033[0;32mPass\033[0m'
-  else 
-      echo -e $f '\033[0;31mFail\033[0m'
-      echo $f >> log
-      echo "------------------------------" >> log
-      echo "got" >> log
-      cat tests/output/$f >> log
-      printf "\n\n" >> log
-      echo "expected" >> log
-      cat tests/cases/$f >> log
-      printf "\n\n" >> log
-      echo "diff" >> log
-      diff <(cat tests/cases/$f) <(cat tests/output/$f) >> log
-      printf "\n\n" >> log
-  fi
-
-done)|column -t
+    done)|column -t
+    
+fi
 
