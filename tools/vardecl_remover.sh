7a2ea0af71f4c0598e1dd640f15d863abdfa2dd3
Troels Leth Jensen <troelslethjensen@gmail.com>
2014-11-06 11:21:05 +0100

    Removing benchmarks again

--- tools/vardecl_remover.sh
+++ tools/vardecl_remover.sh
@@ -7,5 +7,9 @@ dartDir=$(which dart)
 
 for f in $(tools/dependency.dart --dart-sdk ${dartDir%bin/dart} $1)
 do
-    vardecl_transform.dart $f
+    echo $f
+    dart ../DartVarDeclTransform/bin/vardecl_transform.dart -w $f
 done
+
+
+#
