9596a5a853f3dbbe410d85b578204216c8b34ce4
Troels Leth Jensen <troelslethjensen@gmail.com>
2014-12-08 10:14:51 +0100

    working resolving

--- bin/engine.dart
+++ bin/engine.dart
@@ -243,8 +243,8 @@ class Engine {
     new ExportResolver(this, _elementAnalysis);
     new ImportResolver(this, _elementAnalysis);
     
-    new IdentifierResolver(this,  _elementAnalysis);
     new ClassHierarchyResolver(this, _elementAnalysis);
+    new IdentifierResolver(this,  _elementAnalysis);
 
     if (this.options.printAstNodes) {
       unit.accept(new PrintAstVisitor());
